fd9b30c0d1a42794e2789b77988a5f7d71c96859
marielosagui <37224129+marielosagui@users.noreply.github.com>
2018-03-15 22:14:48 -0600

    Add files via upload

---
+++ Prueba1.java
@@ -0,0 +1,23 @@
+
+package prueba1;
+
+/**
+ * @author karla
+ */
+public class Prueba1 {
+
+    public static void main(String []args) {
+     System.out.println("**Datos Personales**");
+     String nombre ="Karla Marielos";
+     String apellido="Aguilar Figueroa";
+     String FechaNac = "14 de Febrero";
+     String edad= "20";
+     String dui ="012345678-9";
+     
+     System.out.println("Nombre: "+nombre);
+     System.out.println("Apellido: "+ apellido);
+     System.out.println("Fecha de Nacimiento: "+ FechaNac);
+     System.out.println("Edad: "+ edad);
+     System.out.println("DUI : "+ dui);
+    }
+}
